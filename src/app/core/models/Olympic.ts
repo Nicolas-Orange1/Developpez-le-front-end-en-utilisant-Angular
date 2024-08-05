500cd690845ef936218e509ddbd3490e8d59a1f0
Nicolas-Orange1 <orange.nicolas76@gmail.com>
2024-08-05 19:20:12 +0200

    Ajout des models Olympic et Participation

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -7,3 +7,12 @@ example of an olympic country:
     participations: []
 }
 */
+import { Participation } from './Participation';
+
+export class Olympic {
+  constructor(
+    public id: number,
+    public country: string,
+    public participations: Participation[]
+  ) {}
+}
